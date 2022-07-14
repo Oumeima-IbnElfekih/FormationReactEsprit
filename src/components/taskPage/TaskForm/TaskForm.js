f4a17bd744f5a1f0e5e62172ba788df4401536c7
Oumeima-IbnElfekih <oumeimaibnelg@gmail.com>
2022-07-14 14:38:30 +0100

    add new lib react-hook-form

--- src/components/taskPage/TaskForm/TaskForm.js
+++ src/components/taskPage/TaskForm/TaskForm.js
@@ -1,20 +1,25 @@
 import { useState } from "react";
 import "./TaskForm.css"
-
+import { useForm } from "react-hook-form";
 function TaskForm(props){
     // props.sayHello("hello Esprit") ; 
     const addTask = "Add a task"
     const steps =["Enter the task title" ,"Click and add task"]
     const [title , setTitle] = useState("learn");
-   
+    const { register, handleSubmit ,watch ,formState :{errors} } = useForm();
     return (
        
        <>
              <ul>
               {steps.map((s,index) => <li key={index}>{s}</li>)}
               </ul> 
-    <input type="text" name="title" value={title} onChange={(e)=> setTitle(e.target.value)}/>
-    <button type="submit" onClick={()=>props.addTask(title)}>{addTask}</button>
+    <input type="text"
+     name="title"
+    //   defaulyValue={title} 
+      {...register("title", {required : true}) }
+   />
+   {errors.title && <span>Required field !!!!!</span>}
+    <button type="submit" onClick={handleSubmit(()=>props.addTask(title))}>{addTask}</button>
     </>
 )
     
